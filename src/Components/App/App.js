7b00fdd6cc705a1515dfd0444cd61c74a7ef3deb
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-20 00:58:51 +0300

    fixed display of HTML elements

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,17 +6,21 @@ function App() {
     <div id="container">
       <Drum />
       <div id="toggle-container">
-        <div className="toggle">
-          <div className="toggle-button button-1">p</div>
+        <div className="text-and-toggle">
+          <span className="text">Power</span>
+          <div className="toggle">
+            <div className="toggle-button">p</div>
+          </div>
         </div>
-        <div id="current-sound">
-          p
-        </div>
+        <div id="current-sound">p</div>
         <div id="change-volume">
           <input type="range" min="0" max="100" />
         </div>
-        <div className="toggle">
-          <div className="toggle-button">p</div>
+        <div className="text-and-toggle">
+          <span className="text">Bank</span>
+          <div className="toggle">
+            <div className="toggle-button">p</div>
+          </div>
         </div>
       </div>
     </div>
