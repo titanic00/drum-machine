9c701efbeb37757ab9fe48fae5bfc9989c28ba6e
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-20 20:16:39 +0300

    added sounds when pressing a keyword button

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,34 +3,54 @@ import { Drum } from "../Drum/Drum";
 import { useState } from "react";
 
 function App() {
-  const [isClicked, clickState] = useState(false);
+  const [isPowerOff, clickState] = useState(true);
+  const [currentSound, setSound] = useState('');
+  const [currentVolume, changeVolume] = useState(0.9);
+  const [isBank, setBank] = useState(false);
 
   const handleClick = () => {
-    if(isClicked) {
-      clickState(false);
+    if(!isPowerOff) {
+      clickState(true);
       return;
     }
-    clickState(true);
+    clickState(false);
+  }
+
+  const setNewSound = newSound => {
+    setSound(newSound);
+  }
+
+  const handleChange = (event) => {
+    changeVolume(event.target.value);
+    setSound(Math.floor(event.target.value * 100));
+  }
+
+  const handleBank = () => {
+    if(isBank) {
+      setBank(false);
+      return;
+    }
+    setBank(true);
   }
 
   return (
     <div id="container">
-      <Drum />
+      <Drum currentSound={setNewSound} powerCheck={isPowerOff} volume={currentVolume} bank={isBank} />
       <div id="toggle-container">
         <div className="text-and-toggle">
           <span className="text">Power</span>
           <div className="toggle">
-            <div className="toggle-button" onClick={handleClick} style={{float: isClicked ? "right" : "left"}}>p</div>
+            <div className="toggle-button" onClick={handleClick} style={{float: isPowerOff ? "left" : "right"}}>p</div>
           </div>
         </div>
-        <div id="current-sound">p</div>
-        <div id="change-volume">
-          <input type="range" min="0" max="100" />
+        <div id="current-sound">{currentSound}</div>
+        <div id="change-volume" onChange={handleChange}>
+          <input type="range" min="0" max="1" step="0.01" />
         </div>
         <div className="text-and-toggle">
           <span className="text">Bank</span>
           <div className="toggle">
-            <div className="toggle-button">p</div>
+            <div className="toggle-button" onClick={handleBank} style={{float: isBank ? "right" : "left"}}>p</div>
           </div>
         </div>
       </div>
--- src/Components/Drum/Drum.js
+++ src/Components/Drum/Drum.js
@@ -1,39 +1,68 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Drum.css";
+import { sounds } from "./sounds";
+import { sounds_bank } from "./sounds";
+
+const sound = new Audio();
 
 export function Drum(props) {
+  const handlePress = (event) => {
+    if (props.bank) {
+      sound.src = sounds_bank[event.key].sound;
+      sound.id = sounds_bank[event.key].id;
+      props.currentSound(sounds_bank[event.key].id);
+    } else {
+      sound.src = sounds[event.key].sound;
+      sound.id = sounds[event.key].id;
+      props.currentSound(sounds[event.key].id);
+    }
+
+    sound.volume = props.volume;
+    sound.pause();
+
+    sound.play();
+  };
+
+  useEffect(() => {
+    if (props.powerCheck) window.addEventListener("keydown", handlePress);
+
+    return function cleanup() {
+      window.removeEventListener("keydown", handlePress);
+    };
+  });
+
   return (
     <div id="drum-container">
       <div className="drum-buttons">
-        <div className="drum-button" id="q">
+        <div className="drum-button" id="Heater 1">
           Q
         </div>
-        <div className="drum-button" id="w">
+        <div className="drum-button" id="Heater 2">
           W
         </div>
-        <div className="drum-button" id="e">
+        <div className="drum-button" id="Heater 3">
           E
         </div>
       </div>
       <div className="drum-buttons">
-        <div className="drum-button" id="a">
+        <div className="drum-button" id="Heater 4">
           A
         </div>
-        <div className="drum-button" id="s">
+        <div className="drum-button" id="Clap">
           S
         </div>
-        <div className="drum-button" id="d">
+        <div className="drum-button" id="Open HH">
           D
         </div>
       </div>
       <div className="drum-buttons">
-        <div className="drum-button" id="z">
+        <div className="drum-button" id="Kick n' Hat">
           Z
         </div>
-        <div className="drum-button" id="x">
+        <div className="drum-button" id="Kick">
           X
         </div>
-        <div className="drum-button" id="c">
+        <div className="drum-button" id="Closed HH">
           C
         </div>
       </div>
