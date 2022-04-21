252e22d07fa394c135fbf6803dbc023434b595ff
titanic00 <98103546+titanic00@users.noreply.github.com>
2022-04-21 18:27:25 +0300

    styled and added readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# In process...
+# About project
+
+This is an application that produces various sounds at the touch of a button.
+
+## How to interact with this app?
+
+In the first section there are buttons, upon pressing of which you will hear a sound.
+<br>
+in the second section there are buttons that perform some special functions.
+<br>
+The "Power" button is responsible for the power supply, by default it is turned on. The "Bank" button is responsible for what sounds will be played when the button is pressed. By default, the button is disabled, enabling it, other sounds will be played.
+<br>
+The slider is responsible for the volume, the sound that just sounded will be registered in a small window.
+
+### 
+If you want to try out my app, follow this link: https://titanic00.github.io/drum-machine/
---
+++ src/Components/Drum/sounds.js
@@ -0,0 +1,77 @@
+export const sounds = {
+    q: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+        id: "Heater 1",
+    },
+    w: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+        id: "Heater 2",
+    },
+    e: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+        id: "Heater 3",
+    },
+    a: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+        id: "Heater 4",
+    },
+    s: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+        id: "Clap",
+    },
+    d: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+        id: "Open HH",
+    },
+    z: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+        id: "Kick n' Hat",
+    },
+    x: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+        id: "Kick",
+    },
+    c: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+        id: "Closed HH",
+    },
+};
+
+export const sounds_bank = {
+    q: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3",
+        id: "Chord 1",
+    },
+    w: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3",
+        id: "Chord 2",
+    },
+    e: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3",
+        id: "Chord 3",
+    },
+    a: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3",
+        id: "Shaker",
+    },
+    s: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3",
+        id: "Open HH",
+    },
+    d: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3",
+        id: "Closed HH",
+    },
+    z: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3",
+        id: "Punchy-Kick",
+    },
+    x: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3",
+        id: "Side-Stick",
+    },
+    c: {
+        sound: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3",
+        id: "Snare",
+    },
+};
